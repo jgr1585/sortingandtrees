75304c027ada3b8c7cafc9553be6c582db06ab43
jgr1585 <jgr1585@students.fhv.at>
2024-04-16 10:26:13 +0200

    Optimized main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 package at.fhv
 
 import at.fhv.objects.AVLTreeNode
-import at.fhv.objects.BinaryTreeNode
 import at.fhv.objects.TreePrinter.printTree
 
 fun main() {
@@ -10,17 +9,18 @@ fun main() {
     println("Enter a number to insert into the tree or 'q' to quit:")
 
     while (true) {
-        val input = readlnOrNull() ?: ""
-        if (input == "q") {
-            break
-        }
-        try {
-            root = root?.insert(input.toInt()) ?: AVLTreeNode(input.toInt())
+        when (val input = readlnOrNull() ?: "") {
+            "c" -> root = null
+            "q" -> break
 
-            root.printTree()
-            root.printList()
-        } catch (e: NumberFormatException) {
-            println("Invalid input. Please enter a number.")
+            else -> try {
+                root = root?.insert(input.toInt()) ?: AVLTreeNode(input.toInt())
+
+                root.printTree()
+                root.printList()
+            } catch (e: NumberFormatException) {
+                println("Invalid input. Please enter a number.")
+            }
         }
     }
 }
